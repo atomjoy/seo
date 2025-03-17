a5ec31a54f1af9557a6d4b860f00a2f59b51b796
Atomjoy <atomjoy.official@gmail.com>
2025-03-17 12:35:40 +0100

    Create AddSchema.vue

---
+++ vue/AddSchema.vue
@@ -0,0 +1,27 @@
+<template></template>
+<script setup>
+import { onMounted } from 'vue';
+
+const props = defineProps({
+	json: {
+		type: [Array, Object],
+		default: [],
+	},
+});
+
+const head = document.querySelector('head');
+const el = document.createElement('script');
+
+onMounted(() => {
+	el.setAttribute('type', 'application/ld+json');
+	el.setAttribute('id', 'structured-data');
+	el.innerText = JSON.stringify(props.json);
+	head.appendChild(el);
+});
+
+function getSchemaObject(id = '#structured-data') {
+	let el = document.querySelector(id);
+	let obj = JSON.parse(el.innerText);
+	return obj;
+}
+</script>
