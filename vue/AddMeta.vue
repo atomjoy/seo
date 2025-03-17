5c2b9076c55effaac5bfb919985f2d80c4385956
Atomjoy <atomjoy.official@gmail.com>
2025-03-17 12:34:53 +0100

    Create AddMeta.vue

---
+++ vue/AddMeta.vue
@@ -0,0 +1,34 @@
+<template></template>
+<script setup>
+import { onMounted } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const { t, locale } = useI18n({ useScope: 'global' });
+
+const props = defineProps({
+	property: {
+		type: String,
+		default: 'og:site_name',
+	},
+	content: {
+		type: String,
+		default: 'Example.com',
+	},
+});
+
+const head = document.querySelector('head');
+const meta = document.createElement('meta');
+
+onMounted(() => {
+	meta.setAttribute('property', props.property);
+	meta.setAttribute('content', t(props.content));
+	head.appendChild(meta);
+});
+</script>
+
+<!--
+// Required in main.js createI18n(options)
+allowComposition: true,
+globalInjection: true,
+legacy: false,
+-->
